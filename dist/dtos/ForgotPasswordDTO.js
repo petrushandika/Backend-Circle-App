b0aabb67d5aaf1ad7e528cda094e254eb7955639
petrushandika <petrushandikasinaga@gmail.com>
2024-07-02 18:43:10 +0700

    Update .gitignore to exclude dist directory

--- dist/dtos/ForgotPasswordDTO.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ForgotPasswordDTO {
-    constructor({ email }) {
-        this.email = email;
-    }
-}
-exports.default = ForgotPasswordDTO;
-//# sourceMappingURL=ForgotPasswordDTO.js.map
